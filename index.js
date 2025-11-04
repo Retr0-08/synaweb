9020f58b7cf727302c81b1c72226e67ac3648628
Retr0-08 <114373611+Retr0-08@users.noreply.github.com>
2025-11-04 14:01:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -43,3 +43,4 @@ app.post('/api/chat', async (req, res) => {
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'public','index.html')));
 
 app.listen(PORT, ()=> console.log(`Syna rodando na porta ${PORT}`));
+
